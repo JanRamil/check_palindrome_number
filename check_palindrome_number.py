61488816aaedb79be8519bfc6ba9d48c37228c8e
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 02:57:27 +0800

    Creating a code that reverse the given number

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -4,5 +4,16 @@
 # Create a pseudocode
 
 # Create a code that will input your desired number
-input_number = int(input("Enter a number: "))
+input_number = int(input("Please enter your desired number: "))
+
+# Create a code that will print the number you entered
+print("The original number is", input_number)
+
+# reverse the given number
+number = input_number
+rev_number = 0
+while number > 0:
+    remainder = number % 10
+    rev_number = (rev_number * 10) + remainder
+    number = number // 10
 
